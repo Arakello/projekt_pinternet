25465b841a0fdd0b6345ff3f444280e289a7f99e
Tomasz Załupski <tzalupski@gmail.com>
2019-01-03 23:19:45 +0100

    Initialize RSpec. Add Picture model validation Spec.

--- spec/controllers/pictures_controller_spec.rb
+++ spec/controllers/pictures_controller_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe PictureController, type: :controller do
+RSpec.describe PicturesController, type: :controller do
 
   describe "GET #new" do
     it "returns http success" do
